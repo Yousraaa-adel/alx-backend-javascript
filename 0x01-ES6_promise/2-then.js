b2423b7363c49fba4e18857e7df3edbd51bc1271
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-08-17 20:03:41 +0300

    Task 2 of ES6 promisses

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  promise
+  return promise
     .then(() => {
       console.log('Got a response from the API');
       return {
@@ -7,5 +7,5 @@ export default function handleResponseFromAPI(promise) {
         body: 'success',
       };
     })
-    .catch(() => Error());
+    .catch(() => new Error());
 }
