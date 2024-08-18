8d435235c53e2deada5fb7304b05a94e67189aa6
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-08-18 12:35:13 +0300

    Task 6 in ES6 Promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,10 +3,13 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results.map((result) => {
-      if (result.status === 'fulfilled') {
-        return { status: result.status, value: result.value };
-      }
-      return { status: result.status, value: result.reason };
-    }));
+    .then((results) => {
+      // console.log('Results:', results);
+      return results.map((result) => {
+        if (result.status === 'fulfilled') {
+          return { status: result.status, value: result.value };
+        }
+        return { status: result.status, value: result.reason };
+      });
+    });
 }
