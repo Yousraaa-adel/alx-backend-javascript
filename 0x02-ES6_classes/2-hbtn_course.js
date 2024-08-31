2a21861aded972d563219a534ef533c67b1f4355
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-08-31 15:09:16 +0300

    Task 2 in ES6 Classes

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,19 @@
+export default class HolbertonCourse {
+  constructor(name, lenght, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this.name;
+  }
+
+  get length() {
+    return this.length;
+  }
+
+  get students() {
+    return this.students;
+  }
+}
